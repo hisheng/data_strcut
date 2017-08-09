2f8a128d856b64a18d697efe440e9ffb544ff0d7
张海生 <465908774@qq.com>
2017-08-09 18:03:42 +0800

    二叉树

---
+++ bitree/bitree.h
@@ -0,0 +1,44 @@
+/*二叉树*/
+
+#ifndef BITREE_H
+#define BITREE_H
+
+typedef struct BiTreeNode_
+{
+	void *data;
+	struct BiTreeNode_ *left;
+	struct BiTreeNode_ *right;
+}BiTreeNode;
+
+
+typedef struct BiTree_
+{
+	int size;
+	int (*compare)(const void *key1,const void *key2);
+	void (*destory)(void *data);
+	BiTreeNode *root;
+}BiTree;
+
+
+void bitree_init(BiTree *tree,void (*destory)(void *data));
+void bitree_destory(BiTree *tree);
+
+int bitree_ins_left(BiTree *tree , BiTreeNode *node,const void *data); 
+int bitree_ins_right(BiTree *tree , BiTreeNode *node,const void *data);
+
+void bitree_rem_left(BiTree *tree,BiTreeNode *node);
+void bitree_rem_right(BiTree *tree,BiTreeNode *node);
+
+int bitree_merge(BiTree *merge,BiTree *left,BiTree *right ,const void *data);
+
+#define bitree_size(tree) ((tree)->size)
+#define bitree_root(tree) ((tree)->root)
+#define bitree_is_eob(node) ((node) == NULL)
+
+#define bitree_is_leaf(node) ((node)->left == NULL && (node)->right == NULL)
+#define bitree_data(node) ((node)->data)
+#define bitree_left(node) ((node)->left)
+#define bitree_right(node) ((node)->right)
+
+
+#endif 
