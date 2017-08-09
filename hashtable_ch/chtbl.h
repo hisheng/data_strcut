f582289c175d30f34bb152317d04fe9e9b0729e8
张海生 <465908774@qq.com>
2017-08-09 16:29:14 +0800

    哈希表\散列表  链式冲突解决法

---
+++ hashtable_ch/chtbl.h
@@ -0,0 +1,32 @@
+/* 哈希表 -  链表处理冲突*/
+
+#ifndef CHTBL_H
+#define CHTBL_H
+
+#include <stdlib.h>
+#include "list.h"
+
+
+typedef struct CHTbl_
+{
+	int buckets;
+	int (*h)(const void *key);
+	int (*match)(const void *key1,const void *key2)
+	void (*destory)(void *data);
+	int size;
+	List *table;
+}CHTbl;
+
+
+
+int chtbl_init(CHTbl *htbl,int buckets,int (*h)(const void *key),int (*match)(const void *key1,const void *key2),void (*destory)(void *data));
+
+
+void chtbl_destory(CHTbl *htbl);
+int chtbl_insert(CHTbl *htbl,const void *data);
+int chtbl_remove(CHTbl *htbl,void **data);
+int chtbl_lookup(const *htbl ,void **data);
+#define chtbl_size(htbl) ((htbl)->size)
+
+
+#endif
