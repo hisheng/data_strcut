7e5cbcf13f87eed39c3d35a9e228562377d06db5
张海生 <465908774@qq.com>
2017-08-10 11:12:25 +0800

    二叉搜索树(avl平衡因子)

---
+++ bistree/bistree.h
@@ -0,0 +1,17 @@
+/*二叉搜索树(avl平衡因子)*/
+
+#ifndef BISTREE_H
+#define BISTREE_H
+
+#include "bitree"
+
+/*定义 平衡因子*/
+
+#define AVL_LFT_HEAVY 1
+#define AVL_BALANCED 0 
+#define AVL_RGT_HEAVY -1
+
+
+
+
+#endif
